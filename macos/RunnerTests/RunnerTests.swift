38749b976c4691b3138a6756406bc4a5fb76c50c
ItzzRio <81822761+Itzz07@users.noreply.github.com>
2024-07-10 17:05:45 +0200

    Add files via upload

---
+++ macos/RunnerTests/RunnerTests.swift
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+import Cocoa
+import FlutterMacOS
+import XCTest
+
+class RunnerTests: XCTestCase {
+
+  func testExample() {
+    // If you add code to the Runner application, consider adding tests here.
+    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
+
+}
+=======
+import Cocoa
+import FlutterMacOS
+import XCTest
+
+class RunnerTests: XCTestCase {
+
+  func testExample() {
+    // If you add code to the Runner application, consider adding tests here.
+    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
+
+}
+>>>>>>> 8f121c7de4636e2da40e33ba2e9ce629efa5fdd6
+=======
+import Cocoa
+import FlutterMacOS
+import XCTest
+
+class RunnerTests: XCTestCase {
+
+  func testExample() {
+    // If you add code to the Runner application, consider adding tests here.
+    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
+
+}
+>>>>>>> 8f121c7de4636e2da40e33ba2e9ce629efa5fdd6
